61ff16a459d66b5cdd043c61c04e31487136a85a
alexberry721 <alexberry721@gmail.com>
2016-10-10 11:06:45 -0700

    Initial Commit

---
+++ Book Exercise 7.5 - Print Distinct Numbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 7.5 - Print Distinct Numbers
+//
+//  Created by ax on 10/10/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
