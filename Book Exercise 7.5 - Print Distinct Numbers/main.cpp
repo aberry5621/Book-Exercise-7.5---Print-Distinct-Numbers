4a033aaf9a31df0825104ed24d03f0c58258a8c7
alexberry721 <alexberry721@gmail.com>
2016-10-10 23:06:12 -0700

    Made that way harder than it needed

--- Book Exercise 7.5 - Print Distinct Numbers/main.cpp
+++ Book Exercise 7.5 - Print Distinct Numbers/main.cpp
@@ -27,29 +27,30 @@ int main() {
     for (int i = 0; i < ARRAY_SIZE; i++) {
         int current_number = input_numbers[i];
         int current_number_occurrences = 0;
-        for (int j = 0; j < ARRAY_SIZE; j++) {
-            if (current_number == input_numbers[j]) {
+        
+        // compare the current number to each distinct number
+        for (int j = 0; j < 10; j++) {
+            // if the current number matches, discard it
+            if (current_number == distinct_numbers[j]) {
                 current_number_occurrences++;
             }
+            
         }
-        if (current_number_occurrences > 1) {
-            // current number is not distinct :(
-            // cout << "current number " << current_number << " is not distinct" << endl;
-        } else if (current_number_occurrences == 1) {
-            // store current number as a distinct value
-            //cout << "current number " << current_number << " is distinct and being stored to distinct_numbers[] position " << distinct_count << endl;
+        
+        if (current_number_occurrences < 1) {
             distinct_numbers[distinct_count] = current_number;
             distinct_count++;
         }
+
     }
     
     // Output
-    cout << "distinct numbers stored: " << endl;
+    cout << "distinct numbers are: ";
     for (int i = 0; i < distinct_count; i++) {
-        cout << distinct_numbers[i] << endl;
+        cout << distinct_numbers[i] << " ";
     }
     
-    cout << "~ FIN ~" << endl;
+    cout << endl;
     
     return 0;
 }
