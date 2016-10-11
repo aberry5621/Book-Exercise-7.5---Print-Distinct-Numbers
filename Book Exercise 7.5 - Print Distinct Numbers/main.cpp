0fe583b999ca4837eda5e6c9d3825b4f7c5c2194
alexberry721 <alexberry721@gmail.com>
2016-10-10 22:49:49 -0700

    Shit, those were the numbers that appeared only once

--- Book Exercise 7.5 - Print Distinct Numbers/main.cpp
+++ Book Exercise 7.5 - Print Distinct Numbers/main.cpp
@@ -15,27 +15,13 @@ int main() {
     
     // Initialize data storage
     const int ARRAY_SIZE = 10;
-    int input_numbers[ARRAY_SIZE];
-    int unique_count = 0;
-    int unique_numbers[10] = {0};
-    
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        input_numbers[i] = 0;
-    }
-    
-    cout << "Input array container initialized: " << endl;
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        cout << input_numbers[i] << endl;
-    }
+    int input_numbers[ARRAY_SIZE] = {0};
+    int distinct_count = 0;
+    int distinct_numbers[10] = {0};
     
     cout << "Input 10 digits: ";
     for (int i = 0; i < ARRAY_SIZE; i++) {
         cin >> input_numbers[i];
-    }
-    
-    cout << "Input array container contents: " << endl;
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        cout << input_numbers[i] << endl;
     }
     
     for (int i = 0; i < ARRAY_SIZE; i++) {
@@ -47,24 +33,23 @@ int main() {
             }
         }
         if (current_number_occurrences > 1) {
-            // current number is not unique :(
-            // cout << "current number " << current_number << " is not unique" << endl;
+            // current number is not distinct :(
+            // cout << "current number " << current_number << " is not distinct" << endl;
         } else if (current_number_occurrences == 1) {
-            // store current number as a unique value
-            //cout << "current number " << current_number << " is unique and being stored to unique_numbers[] position " << unique_count << endl;
-            unique_numbers[unique_count] = current_number;
-            unique_count++;
+            // store current number as a distinct value
+            //cout << "current number " << current_number << " is distinct and being stored to distinct_numbers[] position " << distinct_count << endl;
+            distinct_numbers[distinct_count] = current_number;
+            distinct_count++;
         }
     }
     
     // Output
-    cout << "Unique numbers stored: " << endl;
-    for (int i = 0; i < unique_count; i++) {
-        cout << unique_numbers[i] << endl;
+    cout << "distinct numbers stored: " << endl;
+    for (int i = 0; i < distinct_count; i++) {
+        cout << distinct_numbers[i] << endl;
     }
     
     cout << "~ FIN ~" << endl;
-    
     
     return 0;
 }
